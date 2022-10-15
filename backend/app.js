3c2902f8d1cea9fcc74fc4145e1b9931cc2f440d
Yana Bochkareva <yana.boc@yandex.ru>
2022-10-15 04:36:53 +0400

    feat: crash-test

--- backend/app.js
+++ backend/app.js
@@ -30,6 +30,12 @@ app.use(cors);
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', signIn, login);
 app.post('/signup', signUp, createUser);
 
