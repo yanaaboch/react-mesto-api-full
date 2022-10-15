1878519b1b6cb180ee2fbe530bae138c9be5c115
Yana Bochkareva <yana.boc@yandex.ru>
2022-10-16 01:08:06 +0400

    cors

--- backend/app.js
+++ backend/app.js
@@ -30,15 +30,6 @@ app.use(cors({
   origin: ['http://yanaaboch.mesto.nomoredomains.icu', 'https://yanaaboch.mesto.nomoredomains.icu', 'http://localhost:3000', 'https://localhost:3000'],
   credentials: true,
 }));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-  if (req.method === 'OPTIONS') {
-    res.send(200);
-  }
-  next();
-});
 
 app.use(requestLogger);
 
