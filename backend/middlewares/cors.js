2cc440e88d228060ee981fb7581506528fdf6066
Yana Bochkareva <yana.boc@yandex.ru>
2022-10-15 22:31:47 +0400

    fix: cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -5,20 +5,26 @@ const allowedCors = [
   'https://localhost:3000',
 ];
 
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-module.exports = (req, res, next) => {
+const cors = (req, res, next) => {
   const { origin } = req.headers;
+  // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
-  res.header('Access-Control-Allow-Credentials', true);
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.end();
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
   }
-  next();
+  return next();
 };
+
+module.exports = cors;
